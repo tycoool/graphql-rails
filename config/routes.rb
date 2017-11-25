5b767faf4a43fc3268754b86ca0b7c1395f00a3f
Cristian Brotto <cristian.brotto@gmail.com>
2017-11-25 15:28:39 -0800

    GraphiQL route

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   post "/graphql", to: "graphql#execute"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  mount Graphiql::Rails::Engine, at: "/gi", graphql_path: "/graphql" if Rails.env.development
+  mount GraphiQL::Rails::Engine, at: "/gi", graphql_path: "/graphql" if Rails.env.development?
 end
